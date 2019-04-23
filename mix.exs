f2a38369e7be33e28e079229100c9a2297d55d9c
RaymondLoranger <Raymond.Loranger@bell.net>
2019-04-23 12:51:17 -0400

    0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Islands.State.MixProject do
   def project do
     [
       app: :islands_state,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       name: "Islands State",
@@ -46,7 +46,7 @@ defmodule Islands.State.MixProject do
     [
       {:mix_tasks,
        github: "RaymondLoranger/mix_tasks", only: :dev, runtime: false},
-      {:poison, "~> 4.0"},
+      {:poison, "~> 3.0"},
       {:jason, "~> 1.0"},
       {:persist_config, "~> 0.1"},
       {:islands_player_id, "~> 0.1"},
